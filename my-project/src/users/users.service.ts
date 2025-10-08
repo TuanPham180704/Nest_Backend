e66259d0a8883caf71750fb0dea0a6c06b0093ea
Tuandev18 <phamtuan180704@gmail.com>
2025-10-08 23:44:00 +0700

    Rest API && Test Postmain

--- my-project/src/users/users.service.ts
+++ my-project/src/users/users.service.ts
@@ -2,9 +2,32 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  private user = ['Tuan', 'Giang'];
-
+  private users = [
+    { id: 1, name: 'Tuan', email: 'tuanhihi@gmail.com' },
+    { id: 2, name: 'Giang', email: 'gianghaha@gmail.com' },
+  ];
   findAll() {
-    return this.user;
+    return this.users;
+  }
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+  create(userData: { name: string; email: string }) {
+    const newUser = { id: this.users.length + 1, ...userData };
+    this.users.push(newUser);
+    return newUser;
+  }
+  update(id: number, updateData: { name: string; email: string }) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) return null;
+    Object.assign(user, updateData);
+    return user;
+  }
+  remove(id: number) {
+    const index = this.users.findIndex((u) => u.id === id);
+    if (index === -1) return { message: 'User not found' };
+    const deleted = this.users[index];
+    this.users.splice(index, 1);
+    return { message: 'Deleted successfully', deleted };
   }
 }
